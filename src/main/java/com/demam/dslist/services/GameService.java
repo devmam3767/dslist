2abd310b2cd3bb5069dea6167263e6ca614e5c3e
Marcio Andre Mendes <marcioandre.ti2@gmail.com>
2024-11-16 19:06:19 -0300

    Deploy ready

--- src/main/java/com/demam/dslist/services/GameService.java
+++ src/main/java/com/demam/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.demam.dslist.dto.GameDTO;
 import com.demam.dslist.dto.GameMinDTO;
 import com.demam.dslist.entities.Game;
+import com.demam.dslist.projections.GameMinProjection;
 import com.demam.dslist.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();		
 	}
 }
